5cd672ee718f0214ba5a307ea51d97f94426b9d1
takeru <yamamo9150@gmail.com>
2024-08-04 16:22:25 +0900

    Patch Update

--- src/BaseEnvLoader.php
+++ src/BaseEnvLoader.php
@@ -23,7 +23,7 @@ abstract class BaseEnvLoader
     /**
      * constructor
      */
-    function __construct()
+    public function __construct()
     {
         $dotenv = Dotenv::createImmutable($this->path);
 
