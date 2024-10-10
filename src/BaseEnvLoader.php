4240548574e6b36c49b0dc0f0787737cf753d23f
takeru <yamamo9150@gmail.com>
2024-10-10 17:54:03 +0900

    Patch Update

--- src/BaseEnvLoader.php
+++ src/BaseEnvLoader.php
@@ -2,6 +2,8 @@
 
 namespace YukataRm\EnvLoader;
 
+use YukataRm\Entity\ArrayEntity;
+
 use Dotenv\Dotenv;
 
 /**
@@ -9,7 +11,7 @@ use Dotenv\Dotenv;
  * 
  * @package YukataRm\EnvLoader
  */
-abstract class BaseEnvLoader
+abstract class BaseEnvLoader extends ArrayEntity
 {
     /*----------------------------------------*
      * Constants
@@ -42,140 +44,44 @@ abstract class BaseEnvLoader
      */
     public function __construct()
     {
-        $dotenv = Dotenv::createImmutable($this->path);
+        $this->init();
 
-        $dotenv->safeLoad();
+        $this->setData($_ENV);
 
-        $this->setEnv();
+        $this->bind();
     }
 
-    /**
-     * set environment variables
-     * 
-     * @return void
-     */
-    abstract protected function setEnv(): void;
-
     /*----------------------------------------*
-     * Get Environment Variable
+     * Method
      *----------------------------------------*/
 
     /**
-     * load environment variable
+     * init Dotenv
      * 
-     * @param string $key
-     * @return string|null
+     * @return void
      */
-    protected function getEnv(string $key): string|null
+    protected function init(): void
     {
-        return $_ENV[$key] ?? null;
+        $dotenv = Dotenv::createImmutable($this->path);
+
+        $dotenv->safeLoad();
     }
 
     /**
-     * get environment variable as string
+     * bind .env parameters
      * 
-     * @param string $key
-     * @param string $default
-     * @return string
+     * @return void
      */
-    protected function getEnvString(string $key, string $default = ""): string
-    {
-        return $this->getEnv($key) ?? $default;
-    }
+    abstract protected function bind(): void;
 
     /**
-     * get environment variable as string
-     * if key does not exist, return null
+     * get property
      * 
-     * @param string $key
-     * @return string|null
+     * @param string $name
+     * @return mixed
      */
-    protected function getEnvStringNullable(string $key): string|null
+    public function __get(string $name): mixed
     {
-        return $this->getEnv($key);
-    }
-
-    /**
-     * get environment variable as integer
-     * 
-     * @param string $key
-     * @param int $default
-     * @return int
-     */
-    protected function getEnvInt(string $key, int $default = 0): int
-    {
-        $value = $this->getEnv($key);
-
-        return is_null($value) ? $default : intval($value);
-    }
-
-    /**
-     * get environment variable as integer
-     * if key does not exist, return null
-     * 
-     * @param string $key
-     * @return int|null
-     */
-    protected function getEnvIntNullable(string $key): int|null
-    {
-        $value = $this->getEnv($key);
-
-        return is_null($value) ? null : intval($value);
-    }
-
-    /**
-     * get environment variable as boolean
-     * 
-     * @param string $key
-     * @param bool $default
-     * @return bool
-     */
-    protected function getEnvBool(string $key, bool $default = false): bool
-    {
-        $value = $this->getEnv($key);
-
-        return is_null($value) ? $default : boolval($value);
-    }
-
-    /**
-     * get environment variable as boolean
-     * if key does not exist, return null
-     * 
-     * @param string $key
-     * @return bool|null
-     */
-    protected function getEnvBoolNullable(string $key): bool|null
-    {
-        $value = $this->getEnv($key);
-
-        return is_null($value) ? null : boolval($value);
-    }
-
-    /**
-     * get environment variable as floating point number
-     * 
-     * @param string $key
-     * @param float $default
-     * @return float
-     */
-    protected function getEnvFloat(string $key, float $default = 0.0): float
-    {
-        $value = $this->getEnv($key);
-
-        return is_null($value) ? $default : floatval($value);
-    }
-
-    /**
-     * get environment variable as floating point number
-     * if key does not exist, return null
-     * 
-     * @param string $key
-     * @return float|null
-     */
-    protected function getEnvFloatNullable(string $key): float|null
-    {
-        $value = $this->getEnv($key);
-
-        return is_null($value) ? null : floatval($value);
+        return $this->$name();
     }
 }
